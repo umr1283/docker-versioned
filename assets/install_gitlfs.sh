b2df96ad02744d79921044990fd3fb01c6fd39e2
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2022-02-21 11:02:22 +0100

    feat: git LFS install script

---
+++ assets/install_gitlfs.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# https://github.com/git-lfs/git-lfs/wiki/Installation
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+sudo apt-get install git-lfs
