fd4a081bc584c5ce645b48af86c5caee26fee486
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2022-03-16 11:19:50 +0100

    fix(install_gitlfs): add missing curl

--- assets/install_gitlfs.sh
+++ assets/install_gitlfs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get update && apt-get install -y --no-install-recommends libcurl4-openssl-dev
+apt-get update && apt-get install -y --no-install-recommends curl libcurl4-openssl-dev
 
 # https://github.com/git-lfs/git-lfs/wiki/Installation
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | bash && apt-get install git-lfs
