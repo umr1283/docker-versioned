5ffb0d129e94e8d866630ea7694b8eebc6988b09
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2022-03-30 11:14:05 +0200

    fix: shellcheck and rm not used var

--- assets/install_r.sh
+++ assets/install_r.sh
@@ -5,7 +5,6 @@ R_VERSION=${1:-${R_VERSION:-"latest"}}
 
 apt-get update && apt-get -y install lsb-release
 
-DEBIAN_VERSION=${DEBIAN_VERSION:-$(lsb_release -sc)}
 LANG=${LANG:-en_GB.UTF-8}
 CRAN=${CRAN:-https://cran.r-project.org}
 TZ=${TZ:-Etc/UTC}
@@ -14,71 +13,68 @@ ARCH=$(uname -m)
 
 export DEBIAN_FRONTEND=noninteractive
 
-## Set up and install R
-R_HOME=${R_HOME:-/usr/local/lib/R}
+R_HOME=${R_HOME:-"/usr/local/lib/R"}
 
-apt-get update &&
-  apt-get install -y --no-install-recommends \
-    bash-completion \
-    ca-certificates \
-    devscripts \
-    file \
-    fonts-texgyre \
-    g++ \
-    gfortran \
-    gsfonts \
-    libblas-dev \
-    libbz2-* \
-    libcurl4 \
-    libicu* \
-    liblapack-dev \
-    libpcre2* \
-    libjpeg-turbo* \
-    libpangocairo-* \
-    libpng16* \
-    libreadline8 \
-    libtiff* \
-    liblzma* \
-    make \
-    unzip \
-    zip \
-    zlib1g \
-    tk
+apt-get install -y --no-install-recommends \
+  bash-completion \
+  ca-certificates \
+  devscripts \
+  file \
+  fonts-texgyre \
+  g++ \
+  gfortran \
+  gsfonts \
+  libblas-dev \
+  libbz2-* \
+  libcurl4 \
+  libicu* \
+  liblapack-dev \
+  libpcre2* \
+  libjpeg-turbo* \
+  libpangocairo-* \
+  libpng16* \
+  libreadline8 \
+  libtiff* \
+  liblzma* \
+  make \
+  unzip \
+  zip \
+  zlib1g
 
 BUILDDEPS="curl \
-    default-jdk \
-    libbz2-dev \
-    libcairo2-dev \
-    libcurl4-openssl-dev \
-    libpango1.0-dev \
-    libjpeg-dev \
-    libicu-dev \
-    libpcre2-dev \
-    libpng-dev \
-    libreadline-dev \
-    libtiff5-dev \
-    liblzma-dev \
-    libx11-dev \
-    libxt-dev \
-    perl \
-    rsync \
-    subversion \
-    tcl-dev \
-    tk-dev \
-    texinfo \
-    texlive-extra-utils \
-    texlive-fonts-recommended \
-    texlive-fonts-extra \
-    texlive-latex-recommended \
-    texlive-latex-extra \
-    x11proto-core-dev \
-    xauth \
-    xfonts-base \
-    xvfb \
-    wget \
-    zlib1g-dev"
+  default-jdk \
+  libbz2-dev \
+  libcairo2-dev \
+  libcurl4-openssl-dev \
+  libpango1.0-dev \
+  libjpeg-dev \
+  libicu-dev \
+  libpcre2-dev \
+  libpng-dev \
+  libreadline-dev \
+  libtiff5-dev \
+  liblzma-dev \
+  libx11-dev \
+  libxt-dev \
+  perl \
+  rsync \
+  subversion \
+  tcl-dev \
+  tk-dev \
+  texinfo \
+  texlive-extra-utils \
+  texlive-fonts-recommended \
+  texlive-fonts-extra \
+  texlive-latex-recommended \
+  texlive-latex-extra \
+  x11proto-core-dev \
+  xauth \
+  xfonts-base \
+  xvfb \
+  wget \
+  zlib1g-dev"
 
-apt-get install -y --no-install-recommends $BUILDDEPS
+apt-get install -y --no-install-recommends "$BUILDDEPS"
 
 ## Download R from 0-Cloud CRAN mirror or CRAN
 function download_r_src() {
@@ -111,7 +107,8 @@ R_PAPERSIZE=letter \
   AWK=/usr/bin/awk \
   CFLAGS="-g -O2 -fstack-protector-strong -Wformat -Werror=format-security -Wdate-time -D_FORTIFY_SOURCE=2 -g" \
   CXXFLAGS="-g -O2 -fstack-protector-strong -Wformat -Werror=format-security -Wdate-time -D_FORTIFY_SOURCE=2 -g" \
-  ./configure --enable-R-shlib \
+  ./configure \
+  --enable-R-shlib \
   --enable-memory-profiling \
   --with-readline \
   --with-blas \
@@ -124,24 +121,29 @@ make install
 make clean
 
 ## Add a library directory (for user-installed packages)
-mkdir -p ${R_HOME}/site-library
-chown root:staff ${R_HOME}/site-library
-chmod g+ws ${R_HOME}/site-library
+mkdir -p "${R_HOME}/site-library"
+chown root:staff "${R_HOME}/site-library"
+chmod g+ws "${R_HOME}/site-library"
 
-echo "R_LIBS=\${R_LIBS-'${R_HOME}/site-library:${R_HOME}/library'}" >>${R_HOME}/etc/Renviron.site
-echo 'LANGUAGE="${LANG}"' >>${R_HOME}/etc/Renviron.site
-echo "R_LIBS_USER=~/R/library" >>${R_HOME}/etc/Renviron.site
-echo 'TZ="${TZ}"' >>${R_HOME}/etc/Renviron.site
-echo "R_MAX_NUM_DLLS=300" >>${R_HOME}/etc/Renviron.site
-echo "RENV_PATHS_CACHE=/renv_cache" >>${R_HOME}/etc/Renviron.site
+echo "R_LIBS=\${R_LIBS-'${R_HOME}/site-library:${R_HOME}/library'}
+LANGUAGE=\"${LANG}\"
+R_LIBS_USER=~/R/library
+R_MAX_NUM_DLLS=300
+TZ=\"${TZ}\"
+RENV_PATHS_CACHE=/renv_cache
+" >>"${R_HOME}/etc/Renviron.site"
 
 echo "options(
-  repos = c(CRAN = \"${CRAN}\"),
-  download.file.method = \"libcurl\",
-  HTTPUserAgent = sprintf(\"R/%s R (%s)\", getRversion(), paste(getRversion(), R.version$platform, R.version$arch, R.version$os))
-)" >>${R_HOME}/etc/Rprofile.site
-
-echo 'Sys.umask("0002")' >>${R_HOME}/etc/Rprofile.site
+  repos = c(CRAN = '${CRAN}'),
+  download.file.method = 'libcurl',
+  HTTPUserAgent = sprintf(
+    'R/%s R (%s)',
+    getRversion(),
+    paste(getRversion(), R.version[['platform']], R.version[['arch']], R.version[['os']])
+  )
+)
+Sys.umask(\"0002\")
+" >>"${R_HOME}/etc/Rprofile.site"
 
 if ! dpkg -l | grep -q libopenblas-dev; then
   apt-get update && apt-get install -y --no-install-recommends libopenblas-dev
@@ -152,7 +154,14 @@ fi
 cd /
 rm -rf /tmp/*
 rm -rf R-*
-apt-get remove --purge -y $BUILDDEPS
+apt-get remove --purge -y "$BUILDDEPS"
 apt-get autoremove -y
 apt-get autoclean -y
 rm -rf /var/lib/apt/lists/*
+
+# Check the R info
+echo -e "Check the R info...\n"
+
+R -q -e "sessionInfo()"
+
+echo -e "\nInstall R from source, done!"
